e2c2bb4faef7932fe91c5bfb89bcbec6d9c73dad
SofiaFredriksson <sofiapkf@gmail.com>
2019-11-04 12:28:45 +0000

    done

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,18 +1,25 @@
 import React, { Fragment } from 'react'
 
-const Table = (props) => {
+const Table = ({eatenSushis, budget, noBudget}) => {
 
   const renderPlates = (array) => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div key={index} className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
 
   return (
     <Fragment>
+      {
+      noBudget ?
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You're running low on cash! You have: ${ budget } remaining!
       </h1>
+        :
+      <h1 className="remaining">
+        You have: ${ budget } remaining!
+      </h1>
+      }
       <div className="table">
         <div className="stack">
           {
@@ -21,7 +28,7 @@ const Table = (props) => {
                and renders an empty plate
                for every element in the array
             */
-            renderPlates([])
+            renderPlates(eatenSushis)
           }
         </div>
       </div>
